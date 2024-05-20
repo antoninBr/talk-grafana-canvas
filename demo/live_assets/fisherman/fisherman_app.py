42650edd20bf0d97cd8b1dfa9b6c19d67da259be
Antonin <a.brugnot@groupeonepoint.com>
2024-05-20 16:00:52 +0200

    Work on demo

--- demo/live_assets/fisherman/fisherman_app.py
+++ demo/live_assets/fisherman/fisherman_app.py
@@ -1,14 +1,31 @@
 import random
 import time
-from prometheus_client import start_http_server, Counter
+from flask import Flask, jsonify
+from prometheus_client import start_http_server, Counter, generate_latest
 
 # Initialisation du compteur Prometheus
 fish_counter = Counter('fish_caught', 'Number of fishes caught by the fisherman')
 
-def main():
-    # Démarrage du serveur HTTP pour exposer les métriques Prometheus
-    start_http_server(8000)
-    
+# Initialisation de l'application Flask
+app = Flask(__name__)
+
+# Endpoint pour les métriques Prometheus
+@app.route('/metrics')
+def metrics():
+    return generate_latest()
+
+# Endpoint pour réinitialiser le compteur
+@app.route('/reset', methods=['POST'])
+def reset_counter():
+    global fish_counter
+    fish_counter._value.set(0)  # Réinitialisation du compteur à zéro
+    response = jsonify(message="Counter reset to zero")
+
+    # Enable Access-Control-Allow-Origin
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
+
+def increment_fish_counter():
     while True:
         # Génération d'un nombre aléatoire de poissons pêchés entre 0 et 10
         fish_caught = random.randint(0, 5)
@@ -18,4 +35,12 @@ def main():
         time.sleep(30)
 
 if __name__ == "__main__":
-    main()
+    # Démarrage du serveur HTTP pour les métriques Prometheus
+    start_http_server(8000)
+    
+    # Démarrage de l'incrémentation du compteur dans un thread séparé
+    import threading
+    threading.Thread(target=increment_fish_counter).start()
+    
+    # Démarrage de l'application Flask
+    app.run(host='0.0.0.0', port=5000)
